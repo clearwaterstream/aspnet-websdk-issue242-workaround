7f3a3dda4d7fe64f42825dbd62e5cabeb2c1cebf
Igor Krupin <igor.krupin@coxautoinc.com>
2017-10-16 15:30:31 -0400

    initial commit

---
+++ aspnet-websdk-issue242-workaround/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_websdk_issue242_workaround.Controllers
+{
+    public class HomeController : Controller
+    {
+        IActionResult Index()
+        {
+            return Content("Yup");
+        }
+    }
+}
---
+++ aspnet-websdk-issue242-workaround/gulpfile_publish.js
@@ -0,0 +1,16 @@
+﻿var gulp = require("gulp");
+var replace = require('gulp-replace');
+
+var pkgStagingDir = './obj/Release/net47/win10-x64/PubTmp/Out/';
+
+gulp.task('workaround_242', function () {
+    var web_config = pkgStagingDir + 'web.config';
+
+    /* this is to address https://github.com/aspnet/websdk/issues/242 */
+
+    gulp.src([web_config])
+        .pipe(replace(' arguments="-argFile IISExeLauncherArgs.txt"', ''))
+        .pipe(gulp.dest(pkgStagingDir, { overwrite: true }));
+});
+
+gulp.task('default', ['workaround_242']);
